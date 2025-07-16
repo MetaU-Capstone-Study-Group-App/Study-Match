367de71a32b55520949cb9fc2393db80fcd6e892
Monica Coira <monicacoira@meta.com>
2025-07-16 12:54:20 -0700

    calculate score with missing metrics

--- client/src/utils/CompatibilityScore.jsx
+++ client/src/utils/CompatibilityScore.jsx
@@ -37,41 +37,61 @@ const calculatePairPersonalityScore = async (firstUserId, secondUserId, fetchDat
 const calculateLocationDistance = async (firstUserId, secondUserId, fetchData) => {
     const firstUserCoordinates = await fetchData(`user/address/${firstUserId}`, "GET");
     const secondUserCoordinates = await fetchData(`user/address/${secondUserId}`, "GET");
-    const PI_VALUE = Math.PI;
-    const DEGREES_IN_PI_RADIANS = 180;
-    const RADIUS_OF_EARTH_IN_MILES = 3959;
-    const latitudeDistance = (secondUserCoordinates.latitude - firstUserCoordinates.latitude) * PI_VALUE / DEGREES_IN_PI_RADIANS;
-    const longitudeDistance = (secondUserCoordinates.longitude - firstUserCoordinates.longitude) * PI_VALUE / DEGREES_IN_PI_RADIANS;
-    const firstLatitudeInRadians = firstUserCoordinates.latitude * PI_VALUE / DEGREES_IN_PI_RADIANS;
-    const secondLatitudeInRadians = secondUserCoordinates.latitude * PI_VALUE / DEGREES_IN_PI_RADIANS;
-    const a = Math.pow(Math.sin(latitudeDistance / 2), 2) + Math.pow(Math.sin(longitudeDistance / 2), 2) * Math.cos(firstLatitudeInRadians) * Math.cos(secondLatitudeInRadians);
-    const centralAngle = 2 * Math.asin(Math.sqrt(a));
-    return centralAngle * RADIUS_OF_EARTH_IN_MILES;
+    if (firstUserCoordinates.latitude && firstUserCoordinates.longitude && secondUserCoordinates.latitude && secondUserCoordinates.longitude){
+        const PI_VALUE = Math.PI;
+        const DEGREES_IN_PI_RADIANS = 180;
+        const RADIUS_OF_EARTH_IN_MILES = 3959;
+        const latitudeDistance = (secondUserCoordinates.latitude - firstUserCoordinates.latitude) * PI_VALUE / DEGREES_IN_PI_RADIANS;
+        const longitudeDistance = (secondUserCoordinates.longitude - firstUserCoordinates.longitude) * PI_VALUE / DEGREES_IN_PI_RADIANS;
+        const firstLatitudeInRadians = firstUserCoordinates.latitude * PI_VALUE / DEGREES_IN_PI_RADIANS;
+        const secondLatitudeInRadians = secondUserCoordinates.latitude * PI_VALUE / DEGREES_IN_PI_RADIANS;
+        const a = Math.pow(Math.sin(latitudeDistance / 2), 2) + Math.pow(Math.sin(longitudeDistance / 2), 2) * Math.cos(firstLatitudeInRadians) * Math.cos(secondLatitudeInRadians);
+        const centralAngle = 2 * Math.asin(Math.sqrt(a));
+        return centralAngle * RADIUS_OF_EARTH_IN_MILES;
+    }
+    else {
+        return null;
+    }
 }
 
 // Calculates location compatibility score between two users using the distance between their addresses and the max reasonable distance a user can travel (75 miles)
 const calculateLocationScore = async (firstUserId, secondUserId, fetchData) => {
     const distanceBetweenLocations = await calculateLocationDistance(firstUserId, secondUserId, fetchData);
-    const MAX_DISTANCE_IN_MILES = 75;
-    return 1 - (distanceBetweenLocations / MAX_DISTANCE_IN_MILES);
+    if (distanceBetweenLocations){
+        const MAX_DISTANCE_IN_MILES = 75;
+        return 1 - (distanceBetweenLocations / MAX_DISTANCE_IN_MILES);
+    }
+    else {
+        return null;
+    }
 }
 
 // Uses mean absolute deviation of two class standings to calculate class standing compatibility score
 const calculateClassStandingScore = (firstSchoolInfo, secondSchoolInfo) => {
-    const NUM_OF_CLASS_STANDINGS = 4;
-    const standingDifference = Math.abs(SchoolStanding[secondSchoolInfo.class_standing] - SchoolStanding[firstSchoolInfo.class_standing]);
-    return 1 - (standingDifference / NUM_OF_CLASS_STANDINGS);
+    if (firstSchoolInfo.class_standing && firstSchoolInfo.school && secondSchoolInfo.class_standing && secondSchoolInfo.school){
+        const NUM_OF_CLASS_STANDINGS = 4;
+        const standingDifference = Math.abs(SchoolStanding[secondSchoolInfo.class_standing] - SchoolStanding[firstSchoolInfo.class_standing]);
+        return 1 - (standingDifference / NUM_OF_CLASS_STANDINGS);
+    }
+    else {
+        return null;
+    }
 }
 
 // Returns 1 if users attend the same school and 0 otherwise
 const calculateSchoolScore = (firstSchoolInfo, secondSchoolInfo) => {
-    const firstUserSchool = firstSchoolInfo.school.toLowerCase().replace(/\s/g, '');
-    const secondUserSchool = secondSchoolInfo.school.toLowerCase().replace(/\s/g, '');
-    if (firstUserSchool === secondUserSchool){
-        return 1;
+    if (firstSchoolInfo.class_standing && firstSchoolInfo.school && secondSchoolInfo.class_standing && secondSchoolInfo.school){
+        const firstUserSchool = firstSchoolInfo.school.toLowerCase().replace(/\s/g, '');
+        const secondUserSchool = secondSchoolInfo.school.toLowerCase().replace(/\s/g, '');
+        if (firstUserSchool === secondUserSchool){
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
     else {
-        return 0;
+        return null;
     }
 }
 
@@ -79,37 +99,53 @@ const calculateSchoolScore = (firstSchoolInfo, secondSchoolInfo) => {
 const calculateGoalsScore = async (firstUserId, secondUserId, fetchData) => {
     const firstUserGoals = await fetchData(`user/userGoals/${firstUserId}`, "GET");
     const secondUserGoals = await fetchData(`user/userGoals/${secondUserId}`, "GET");
-    const firstUserGoalIds = [];
-    for (const goal of firstUserGoals){
-        firstUserGoalIds.push(goal.goal_id)
+    if (firstUserGoals && secondUserGoals){
+        const firstUserGoalIds = [];
+        for (const goal of firstUserGoals){
+            firstUserGoalIds.push(goal.goal_id)
+        }
+        const secondUserGoalIds = [];
+        for (const goal of secondUserGoals){
+            secondUserGoalIds.push(goal.goal_id)
+        }
+        const firstUserSet = new Set(firstUserGoalIds);
+        const secondUserSet = new Set(secondUserGoalIds);
+        const unionSet = firstUserSet.union(secondUserSet);
+        const intersectionSet = firstUserSet.intersection(secondUserSet);
+        return intersectionSet.size / unionSet.size;
     }
-    const secondUserGoalIds = [];
-    for (const goal of secondUserGoals){
-        secondUserGoalIds.push(goal.goal_id)
+    else {
+        return null;
     }
-    const firstUserSet = new Set(firstUserGoalIds);
-    const secondUserSet = new Set(secondUserGoalIds);
-    const unionSet = firstUserSet.union(secondUserSet);
-    const intersectionSet = firstUserSet.intersection(secondUserSet);
-    return intersectionSet.size / unionSet.size;
 }
 
 // Uses the five metrics above and their respective weights to calculate overall compatibility score
 const calculateOverallCompatibilityScore = async (firstUserId, secondUserId, fetchData) => {
+    const scores = [];
     const personalityScore = await calculatePairPersonalityScore(firstUserId, secondUserId, fetchData);
-    const locationScore = await calculateLocationScore(firstUserId, secondUserId, fetchData); 
+    scores.push(personalityScore * ScoreWeights["personality"]);
+    const locationScore = await calculateLocationScore(firstUserId, secondUserId, fetchData);
+    if (locationScore){
+        scores.push(locationScore * ScoreWeights["location"]);
+    } 
     const firstUserSchoolInfo = await fetchData(`user/schoolInfo/${firstUserId}`, "GET");
     const secondUserSchoolInfo = await fetchData(`user/schoolInfo/${secondUserId}`, "GET");
     const classStandingScore = calculateClassStandingScore(firstUserSchoolInfo, secondUserSchoolInfo);
+    if (classStandingScore){
+        scores.push(classStandingScore * ScoreWeights["class_standing"]);
+    }
     const schoolScore = calculateSchoolScore(firstUserSchoolInfo, secondUserSchoolInfo);
+    if (schoolScore){
+        scores.push(schoolScore * ScoreWeights["school"]);
+    }
     const goalsScore = await calculateGoalsScore(firstUserId, secondUserId, fetchData);
-    return (
-        (personalityScore * ScoreWeights["personality"]) + 
-        (locationScore * ScoreWeights["location"]) + 
-        (classStandingScore * ScoreWeights["class_standing"]) + 
-        (schoolScore * ScoreWeights["school"]) + 
-        (goalsScore * ScoreWeights["goals"])
-    );
+    if (goalsScore){
+        scores.push(goalsScore * ScoreWeights["goals"]);
+    }
+    const overallScore = scores.reduce((a, b) => {
+        return a + b
+    }, 0);
+    return overallScore;
 }
 
 const CompatibilityScore = async (firstUserId, secondUserId, fetchData) => {
